0ca19d957c54ca7694b23ce041f34e12540e138b
Sudhasubbumuthu <115452718+Sudhasubbumuthu@users.noreply.github.com>
2023-12-18 22:30:12 +0530

    Create 007-valid-sudoku.java

---
+++ 007-valid-sudoku.java
@@ -0,0 +1,18 @@
+//36. Valid Sudoku
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                char cur = board[i][j];
+                if(cur != '.')
+                {
+                    if(!seen.add(cur+"found in row"+i)||
+                    !seen.add(cur+"found in col"+j)||
+                    !seen.add(cur+"found in sub box"+i/3+"-"+j/3))return false;
+                }
+            }
+        }
+        return true;
+    }
+}
