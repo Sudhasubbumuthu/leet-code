e2f7196cd3cf6dfe421db9876e23291c81a47434
Sudhasubbumuthu <115452718+Sudhasubbumuthu@users.noreply.github.com>
2024-01-09 21:54:14 +0530

    Create 019-reorder-list.java

---
+++ 019-reorder-list.java
@@ -0,0 +1,35 @@
+// 143. Reorder List
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        ListNode prev = slow.next = null;
+        ListNode nextnode;
+        while (second != null) {
+            nextnode = second.next;
+            second.next = prev;
+            prev = second;
+            second = nextnode;
+        }
+
+        ListNode first = head;
+        second = prev;
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+
+    }
+}
